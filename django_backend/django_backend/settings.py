6a0863e8b4207b224a21f9380cee42661c00d806
hasibul-miraz <hasibul.miraz@gmail.com>
2024-07-09 21:59:16 +1000

    adding .env file
    successfully added .env to store critical information using dotenv and os library

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
+# loading the .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3_@o@dmrxyx_#tds%%0_$t9t2b3%muo)w3cgfdc%z4bc#q#%ps"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
